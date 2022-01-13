85cd8570d28ed68c515a37b3795e7c02a3b1e39a
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-12 17:22:43 -0800

    Install mongoose and connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const { 
 	getNewBeverlyScreenings, 
 	getBrainDeadScreenings, 
@@ -7,11 +9,14 @@ const {
 
 (async () => {
 	try {
+		await mongoose.connect(process.env.MONGODB_URI_TEST);
+		console.log("Connected to MongoDB!");
+		
 		// await getNewBeverlyScreenings();
 		// await getBrainDeadScreenings();
 		// only use one of the two for getting aero screenings
 		// await getAeroTheaterScreeningsUsingFile();
-		await getAeroTheaterScreenings();
+		// await getAeroTheaterScreenings();
 	} catch (error) {
 		console.log(error);
 	}
